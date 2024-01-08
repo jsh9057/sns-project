1b57c5d2c664e24b105ba1e64b1a5e65c9d1ff28
jsh9057 <jsh9057@skuniv.ac.kr>
2024-01-08 22:46:12 +0900

    feat: UserJoin Impl

--- src/main/java/dev/be/sns/exception/SnsApplicationException.java
+++ src/main/java/dev/be/sns/exception/SnsApplicationException.java
@@ -1,5 +1,22 @@
 package dev.be.sns.exception;
 
-// TODO : implement
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public class SnsApplicationException extends RuntimeException {
+
+    private ErrorCode errorCode;
+    private String message;
+
+    @Override
+    public String getMessage() {
+        if(message == null){
+            return errorCode.getMessage();
+        }
+
+        return String.format("%s. %s", errorCode.getMessage(), message);
+    }
 }
+// TODO : implement
