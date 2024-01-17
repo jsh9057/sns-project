5de9ee2065851c7a720c013c5e7f7ea7cc1491a9
jsh9057 <jsh9057@skuniv.ac.kr>
2024-01-17 22:13:57 +0900

    feat: 피드목록기능 테스트및 기능 개발

--- src/main/java/dev/be/sns/repository/PostEntityRepository.java
+++ src/main/java/dev/be/sns/repository/PostEntityRepository.java
@@ -1,10 +1,15 @@
 package dev.be.sns.repository;
 
 import dev.be.sns.model.entity.PostEntity;
+import dev.be.sns.model.entity.UserEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostEntityRepository extends JpaRepository<PostEntity, Integer> {
+
+    Page<PostEntity> findAllByUser(UserEntity entity, Pageable pageable);
 }
 
