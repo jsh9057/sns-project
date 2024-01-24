d96dc909c3044873c3e2c8cd4f3d973cedba844c
jsh9057 <jsh9057@skuniv.ac.kr>
2024-01-24 21:53:38 +0900

    refactor: sse를 사용하여 알람 개선
    * sse를 사용하여 알람이 발생했을 때, 알람 대상 브라우저에 api를 호출 할 수 있도록 수정함.
    * 단일 서버기준에서는 local cache를 사용해도 문제가 없겠지만 여러 서버에서 서로다른 클라이언트를 가지고있다면 문제가 발생할 수 있음

--- src/main/java/dev/be/sns/controller/UserController.java
+++ src/main/java/dev/be/sns/controller/UserController.java
@@ -9,6 +9,7 @@ import dev.be.sns.controller.response.UserLoginResponse;
 import dev.be.sns.exception.ErrorCode;
 import dev.be.sns.exception.SnsApplicationException;
 import dev.be.sns.model.User;
+import dev.be.sns.service.AlarmService;
 import dev.be.sns.service.UserService;
 import dev.be.sns.util.ClassUtils;
 import lombok.RequiredArgsConstructor;
@@ -16,6 +17,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @RestController
 @RequestMapping("api/v1/users")
@@ -23,6 +25,7 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     private final UserService userService;
+    private final AlarmService alarmService;
 
     // TODO : implement
     @PostMapping("/join")
@@ -43,5 +46,14 @@ public class UserController {
                 ()-> new SnsApplicationException(ErrorCode.INTERNAL_SERVER_ERROR,
                     "Casting to User class failed"));
         return Response.success(userService.alarmList(user.getId(), pageable).map(AlarmResponse::fromAlarm));
+    }
+
+    @GetMapping("/alarm/subscribe")
+    public SseEmitter subscribe(Authentication authentication) {
+        User user = ClassUtils.getSafeCastInstance(authentication.getPrincipal(), User.class).orElseThrow(
+                ()-> new SnsApplicationException(ErrorCode.INTERNAL_SERVER_ERROR,
+                        "Casting to User class failed"));
+
+        return alarmService.connectAlarm(user.getId());
     }
 }
